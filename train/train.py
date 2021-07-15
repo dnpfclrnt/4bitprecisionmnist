eb30fa1016d3406acbc9d1221f85f181cf3c165a
dnpfclrnt <83463232+dnpfclrnt@users.noreply.github.com>
2021-07-15 11:25:02 +0900

    Create train.py

---
+++ train/train.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+
+class conv_layer:
+    def __init__(self):
+    

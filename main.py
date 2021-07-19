0752a29540920a2982a217e036e4cac5a74cbaa9
dnpfclrnt <dnpfclrnt@gmail.com>
2021-07-20 00:45:57 +0900

    [#6] main.py changed
    [#6] gen_data updated
    
    [#6] syntax error on main.py fixed
    
    [#6] syntax error on main.py fixed2
    
    [#6] syntax error on main.py fixed3
    
    [#6] syntax error on main.py fixed4
    
    [#6] syntax error on main.py fixed5
    
    [#6] train_config added
    
    [#6] datset changed to torch
    
    [#6] dataset configuration
    
    [#6] dataset configuration

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from dataclasses import dataclass
 
 BIT_CONFIG_FILE = 'bit_config.json'
+DATASET_CONFIG_FILE = 'dataset_config.json'
 TRAIN_CONFIG_FILE = 'train_config.json'
 
 
@@ -15,6 +16,9 @@ class Configs:
             os.path.join(config_root, BIT_CONFIG_FILE), versions[0]
         )
         try:
+            self.dataset_config = load_config(
+                os.path.join(config_root, DATASET_CONFIG_FILE), versions[2]
+            )
             self.train_config = load_config(
                 os.path.join(config_root, TRAIN_CONFIG_FILE), versions[1]
             )
@@ -24,8 +28,8 @@ class Configs:
 
 
 def load_config(config_path, version):
-    with open(config_path, 'rb') as file:
-        config_total = json.load(file)
+    with open(config_path, 'rb') as f:
+        config_total = json.load(f)
     result = config_total[version]
     if config_total.get('roots') is not None:
         result.update(config_total['roots'])
@@ -33,6 +37,12 @@ def load_config(config_path, version):
         result.update(config_total['links'])
     result['version'] = version
     return result
+
+
+def create_data(config_list, save: bool = True):
+    from gen_data import DatasetGenerator
+    gen_data = DatasetGenerator(config_list.dataset_config, save=save)
+    return gen_data.gen_dataset()
 
 
 def train(config_list, save_model=False):
@@ -48,7 +58,7 @@ def parse_ver(version_raw):
     Args:
         version_raw: str
             period split values, e.g. 0.0.0.0
-            bit_config_version . train_config_version . maybe data augmentation . extra bit
+            bit_config_version . train_config_version . data . extra bit
     Returns: dataclass versions
         list of  str versions for each modes
     """
@@ -64,18 +74,20 @@ def parse_ver(version_raw):
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('-m', '--mode', type=str,
-                   choices=['bit', 'train', 'all'],
+                   choices=['data', 'bit', 'train', 'all'],
                    default='all')
     p.add_argument('-v', '--version', type=str, default='0.0.0.0')
-    p.add_argument('config_root', type=str, default='./assets')
-    p.add_argument('s', '--save', type=bool, default=False)
+    p.add_argument('--config_root', type=str, default='./assets')
+    p.add_argument('-s', '--save', type=bool, default=False)
     p.add_argument('--save_model', type=bool, default=False)
 
     args = p.parse_args()
 
     parsed_ver = parse_ver(args.version)
     config_list = Configs(config_root=args.config_root, versions=parsed_ver)
-    if args.mode == 'bit':
+    if args.mode == 'data':
+        create_data(config_list)
+    elif args.mode == 'bit':
         quantize(config_list)
     elif args.mode == 'train':
         train(config_list)
